09d198ed35d2f9834aa0ffe2c81d34b60e6313da
iverbitskyi <ivan.verbitskyj@gmail.com>
2023-12-07 18:18:24 +0200

    fix: add env

--- src/api/axios.api.ts
+++ src/api/axios.api.ts
@@ -1,7 +1,15 @@
-// http://localhost:8001/api/{NeededService}
-
+import dotenv from 'dotenv';
 import axios from 'axios';
 
+// import types for process
+declare var process: {
+  env: {
+    BASE_URL: string;
+  };
+};
+
+dotenv.config();
+
 export const instance = axios.create({
-  baseURL: 'http://localhost:8001/api',
+  baseURL: process.env.BASE_URL,
 });
