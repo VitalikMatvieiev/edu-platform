1137f83284356234c1288310be59df81979376f7
Vlad-2109 <vladswim21@gmail.com>
2023-12-14 23:51:52 +0200

    create-search-component

---
+++ src/pages/mainPage/mainPage.tsx
@@ -0,0 +1,12 @@
+import HeaderMain from '../../components/headerMain/headerMain';
+import './_mainPage.scss';
+
+const MainPage: React.FC = () => {
+  return (
+    <div>
+      <HeaderMain />
+    </div>
+  );
+};
+
+export default MainPage;
