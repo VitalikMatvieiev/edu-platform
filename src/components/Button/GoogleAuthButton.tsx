e91830360f3211a883209927a93400bdd9b1ab52
tanyaivattt <tanyaivatt@gmail.com>
2023-12-19 21:18:49 +0200

    fix indents

--- src/components/Button/GoogleAuthButton.tsx
+++ src/components/Button/GoogleAuthButton.tsx
@@ -1,9 +1,7 @@
 import {useGoogleLogin} from '@react-oauth/google';
 import axios from "axios"
 import './_button.scss';
-// import {FcGoogle} from "react-icons/fc"
 import GoogleIcon from '/src/img/google-icon.svg';
-
 
 
 function GoogleAuthButton() {
@@ -11,21 +9,20 @@ function GoogleAuthButton() {
   const loginGoogle = useGoogleLogin({
     onSuccess: async response => {
       try {
-          const res = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
-              headers: {
-                  "Authorization": `Bearer ${response.access_token}`
-              }
-          })
-          console.log(res.data)
+        const res = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
+          headers: {
+            "Authorization": `Bearer ${response.access_token}`
+          }
+        })
+        console.log(res.data)
       } catch (err) {
-          console.log(err)
+        console.log(err)
       }
     }
   });
   return (
     <>
       <button className="btn-google" onClick={() => loginGoogle()}>
-        {/* <FcGoogle/> */}
         <img src={GoogleIcon} alt="Google icon" />
         <p>Sign in with Google</p>
       </button>
