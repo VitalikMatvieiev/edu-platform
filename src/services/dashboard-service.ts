1c76ef4008e4b65ed73d49326d792089b6460213
Vlad-2109 <vladswim21@gmail.com>
2024-01-13 18:09:40 +0300

    add service for course-detail

---
+++ src/services/dashboard-service.ts
@@ -0,0 +1,16 @@
+import { instance } from './requests';
+import { ICourseDetailsResponseData } from '../types/types';
+
+export const DashboardService = {
+  
+    async getProfile(): Promise<ICourseDetailsResponseData | undefined> {
+      try {
+        const { data } = await instance.get<ICourseDetailsResponseData>('course-details');
+        return data;
+      } catch (error) {
+        console.error('Get course-detail error:', error);
+        throw new Error('Failed to get course-detail');
+      }
+    },
+  };
+  
