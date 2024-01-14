763a321e2c541db60606b48d8234e8b2b21d8fc8
tanyaivattt <tanyaivatt@gmail.com>
2024-01-15 01:21:21 +0200

    style components; add unit tests

--- src/components/dashboard/course-progress/course-progress.tsx
+++ src/components/dashboard/course-progress/course-progress.tsx
@@ -13,28 +13,28 @@ export const CourseProgress = ({
       ? completedChapter / allChapter * 100
       : 0;
     return(
-        <div>
-            <LinearProgress
-            valueBuffer={100}
-            value={progressPercent}
-            variant='buffer'
-            sx={{marginBottom:"7px", borderRadius: '20px',
-              '& .MuiLinearProgress-bar1Buffer': {
-                backgroundColor: customColor1, 
-              },
-              '.MuiLinearProgress-bar2Buffer': {
-                backgroundColor: customColor2,
-              },
-            }}
-            />
-            <Box sx={{display: "flex", justifyContent:"space-between"}}>
-              <Box sx={{}}>
-                {completedChapter}/{allChapter} lessons
-              </Box>
-              <Box>
-                {Math.round(progressPercent)}% completed
-              </Box>
-            </Box>
-        </div>
+      <div>
+        <LinearProgress
+          valueBuffer={100}
+          value={progressPercent}
+          variant='buffer'
+          sx={{marginBottom:"7px", borderRadius: '20px',
+            '& .MuiLinearProgress-bar1Buffer': {
+              backgroundColor: customColor1, 
+            },
+            '.MuiLinearProgress-bar2Buffer': {
+              backgroundColor: customColor2,
+            },
+          }}
+        />
+        <Box sx={{color: "gray", fontSize: ".85em", display: "flex", justifyContent:"space-between"}}>
+          <Box sx={{}}>
+            {completedChapter}/{allChapter} lessons
+          </Box>
+          <Box>
+            {Math.round(progressPercent)}% completed
+          </Box>
+        </Box>
+      </div>
     )
-}
+  }
---
+++ src/components/dashboard/enrolled-card-of-course/_test_/EnrolledCardOfCourse.spec.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import EnrolledCardOfCourse from '../enrolled-card-of-course';
+
+describe('EnrolledCardOfCourse Component', () => {
+  beforeEach(() => {
+    render(
+      <MemoryRouter>
+            <EnrolledCardOfCourse 
+              id=""
+              title=""
+              imageUrl=""
+              category=""
+              timeOfClasses=""
+              numberOfStudents= {0}
+              completedChapterLength= {0}
+              chaptersLength= {0}
+          />
+      </MemoryRouter>
+    );
+  });
+
+  test('renders course title', () => {
+    const courseTitle = screen.getByRole('course-title');
+
+    expect(courseTitle).toBeInTheDocument();
+  });
+
+  test('renders course photo', () => {
+    const coursePhoto = screen.getByAltText('Course Photo');
+  
+    expect(coursePhoto).toBeInTheDocument();
+  });
+});
